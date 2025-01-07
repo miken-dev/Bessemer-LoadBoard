7eaca4a7fbbfd511a41b1c833f5522f715c7fc40
Mike Neal <mike@miken.dev>
2025-01-07 08:08:59 -0500

    add flowbite config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,13 +2,30 @@ import containerQueries from '@tailwindcss/container-queries';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
+import flowbitePlugin from 'flowbite/plugin';
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-
+	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
+	darkMode: 'selector',
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				//flowbite-svelte
+				primary: {
+					50: '',
+					100: '',
+					200: '',
+					300: '',
+					400: '',
+					500: '',
+					600: '',
+					700: '',
+					800: '',
+					900: '',
+				}
+			}
+		}
 	},
 
-	plugins: [typography, forms, containerQueries]
+	plugins: [typography, forms, containerQueries, flowbitePlugin]
 } satisfies Config;
