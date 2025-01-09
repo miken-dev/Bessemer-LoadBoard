4dd3e6cbe3f85da46452e5fc3875b4556f4705c0
Michael Neal <mike@miken.dev>
2025-01-09 10:55:49 -0500

    create developer mode banner

--- src/routes/DevBanner.svelte
+++ src/routes/DevBanner.svelte
@@ -4,6 +4,14 @@
 
 {#if dev}
 	<div>
-
+		<p>Developer mode Backend: Dev - DB: Undefined - Upstream: Undefined - SVKit: Dev<p>
 	</div>
 {/if}
+
+<style>
+	div {
+		text-align: center;
+		height: 30px;
+		background-color: red;
+	}
+</style>
