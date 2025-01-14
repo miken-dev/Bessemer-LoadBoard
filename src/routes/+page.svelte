66a66c91f9edcd6ca7b11b51cfb7d39e8e173df9
Michael Neal <mike@miken.dev>
2025-01-14 05:34:28 -0500

    update main layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import DevBanner from './DevBanner.svelte';
-	import LoadTable from './LoadTable.svelte';
- 
+
 	//import { slide } from "svelte/transition";
 	//import { ChevronDown } from "lucide-svelte";
-	import DataTable from "./LoadTablev2.svelte";
-	import tableData from "$lib/sampledata.json";
+	import DataTable from './LoadTablev2.svelte';
+	import tableData from '$lib/sampledata.json';
 
+	import Map from './Map.svelte';
+	import ViewsBar from '$lib/components/ViewsBar.svelte';
 	// Store selected row id
 	let selectedRow: string | null = $state(null);
 
@@ -14,15 +15,27 @@
 	const toggleDetails = (id: string) => {
 		selectedRow = selectedRow === id ? null : id;
 	};
+
+	let tableIsShowing: boolean = $state(true);
+	let mapIsShowing: boolean = $state(true);
 </script>
 
-<div class="w-full min-h-screen bg-gray-50 text-gray-900 p-4 md:p-8">
-	<div class="max-w-[95rem] mx-auto">
-		<h1 class="text-2xl font-bold mb-6">Data Overview</h1>
-		<DataTable {tableData} {selectedRow} {toggleDetails} />
-	</div>
-</div>
-
 <DevBanner />
+<main class="mx-5">
+	<ViewsBar bind:tableIsShowing bind:mapIsShowing />
+	<div class="flex flex-col items-start md:flex-row">
+		{#if tableIsShowing}
+			<div class="min-h-screen w-full bg-gray-50 p-4 text-gray-900 md:p-8">
+				<div class="mx-auto max-w-[95rem]">
+					<DataTable {tableData} {selectedRow} {toggleDetails} />
+				</div>
+			</div>
+		{/if}
 
-<LoadTable />
+		{#if mapIsShowing}
+			<div class="sticky top-0">
+				<Map />
+			</div>
+		{/if}
+	</div>
+</main>
