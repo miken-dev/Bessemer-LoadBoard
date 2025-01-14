22fee64bbcdff3783df0f6f30c91e4dad1effc10
Michael Neal <mike@miken.dev>
2025-01-14 05:34:02 -0500

    create view bar component

---
+++ src/lib/components/ViewsBar.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	let {
+		tableIsShowing = $bindable(),
+		mapIsShowing = $bindable()
+	}: {
+		tableIsShowing: boolean;
+		mapIsShowing: boolean;
+	} = $props();
+</script>
+
+<div class="flex">
+	<button
+		onclick={() => {
+			tableIsShowing = !tableIsShowing;
+		}}>Table</button
+	>
+	<button onclick={() => {
+		mapIsShowing = mapIsShowing === true ? false : true
+	}}>Map</button>
+</div>
