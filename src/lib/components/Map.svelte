0e3126758491752f1aa6dd532716e84df5f25f52
Michael Neal <mike@miken.dev>
2025-01-15 22:50:00 -0500

    create map component

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -1,2 +1,25 @@
-<h1>Map</h1>
-<img src="https://placehold.co/600x400" />
+<script lang="ts">
+	import { Map, TileLayer, Marker, Popup } from 'sveaflet';
+
+	let marker: Marker;
+
+</script>
+
+<div style="width:100%;height:500px;">
+	<Map
+		options={{
+			center: [51.505, -0.09],
+			zoom: 13
+		}}
+	>
+		<TileLayer url={'https://tile.openstreetmap.org/{z}/{x}/{y}.png'} />
+		<Marker latLng={[51.505, -0.09]} bind:instance={marker}>
+			<Popup
+				options={{
+					content: 'Hello Sveaflet.'
+				}}
+			></Popup>
+		</Marker>
+	</Map>
+</div>
+
