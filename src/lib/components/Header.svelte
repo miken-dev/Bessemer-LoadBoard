26228411c45e83a7f10227895f74de63940f74d3
Michael Neal <mike@miken.dev>
2025-01-15 22:49:37 -0500

    update placeholder images

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,7 +3,7 @@
 
 <div class="flex justify-between items-center w-screen p-5 bg-blue-600" >
 	<div>
-		<img src="https://placehold.co/300x70" />
+		<img src="https://placehold.co/300x70/ff00ff/ffffff"/>
 	</div>
-		<button class="bg-gray-500 hover:bg-blue-600 text-white font-boldd py-2 px-4 rounded">Equipment Search</button>
+		<button class="bg-gray-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Equipment Search</button>
 </div>
