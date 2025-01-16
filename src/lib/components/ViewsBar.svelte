0a20a07fe24342ef1e2dd19fbc66d1ecefa543e6
Michael Neal <mike@miken.dev>
2025-01-15 22:50:06 -0500

    update styling

--- src/lib/components/ViewsBar.svelte
+++ src/lib/components/ViewsBar.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	let active = 'bg-blue-600'
+	let inactive = 'bg-gray-500'
 	let {
 		tableIsShowing = $bindable(),
 		mapIsShowing = $bindable()
@@ -8,13 +10,15 @@
 	} = $props();
 </script>
 
-<div class="flex justify-items-end bg-red-500">
-	<button
+<div class="flex justify-end ">
+	<p class="my-auto">Show:</p>
+	<button class="{tableIsShowing ? active : inactive } text-white py-2 px-4 my-3 mx-1 rounded"
 		onclick={() => {
 			tableIsShowing = !tableIsShowing;
 		}}>Table</button
 	>
-	<button onclick={() => {
+	<button class="{mapIsShowing ? active : inactive } text-white py-2 px-4 my-3 mx-1 rounded"
+		onclick={() => {
 		mapIsShowing = mapIsShowing === true ? false : true
 	}}>Map</button>
 </div>
