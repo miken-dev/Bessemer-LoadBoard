874e9268520746cb5257ca77aaedce4f014e3a86
Michael Neal <mike@miken.dev>
2025-01-16 23:58:08 -0500

    add dark mode button

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
+	import { DarkMode } from 'flowbite-svelte';
 </script>
 
-<div class="flex justify-between items-center w-screen p-5 bg-blue-600" >
+<div class="flex w-screen items-center justify-between bg-blue-600 p-5">
 	<div>
-		<img src="https://placehold.co/300x70/ff00ff/ffffff"/>
+		<img src="https://placehold.co/300x70/ff00ff/ffffff" />
 	</div>
-		<button class="bg-gray-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Equipment Search</button>
+	<div>
+		<button class="rounded bg-gray-500 px-4 py-2 font-bold text-white hover:bg-blue-600"
+			>Equipment Search</button
+		>
+		<DarkMode dark:text-gray-100 />
+	</div>
 </div>
