65b040d9117652141b2acc38fd0f7f0831a0f639
Michael Neal <mike@miken.dev>
2025-01-17 21:40:56 -0500

    update styling

--- src/lib/components/Drawer.svelte
+++ src/lib/components/Drawer.svelte
@@ -8,11 +8,13 @@
 	let {
 		tableData,
 		selectedRow = $bindable(),
-		detailsHidden = $bindable()
+		detailsHidden = $bindable(),
+		tableClicked
 	}: {
 		tableData: TableDataTypes[];
 		selectedRow: number | null;
 		detailsHidden: boolean;
+		tableClicked: boolean;
 	} = $props();
 	let transitionParams = {
 		x: -320,
@@ -48,22 +50,22 @@
 				return 'th';
 		}
 	};
-	let selectedCity = $state("")
-	let selectedState = $state("")
-	let multiples = $state(false)
+	let selectedCity = $state('');
+	let selectedState = $state('');
+	let multiples = $state(false);
 </script>
 
 <Drawer transitionType="fly" {transitionParams} bind:hidden={detailsHidden} id="loadDetailDrawer">
-	<div class="flex items-center flex-col">
+	<div class="flex flex-col items-center">
 		<!-- Inline map -->
-		{#if multiples}
-		<h2>Multiple available in {selectedCity} {selectedState}</h2>
+		{#if multiples && !tableClicked}
+			<h2>Multiple available in {selectedCity} {selectedState}</h2>
 		{/if}
 		{#each tableData as data}
 			{#if data.loadID == selectedRow}
-				{selectedCity = data.originCityName}
-				{selectedState = data.originStateName}
-				<div class="size-80 top-0">
+				{(selectedCity = data.originCityName)}
+				{(selectedState = data.originStateName)}
+				<div class="top-0 size-80">
 					<DrawerMap
 						originLat={data.originLat}
 						originLng={data.originLng}
@@ -118,7 +120,6 @@
 					</div>
 					<div>
 						<h3>Revenue</h3>
-
 					</div>
 					<div>
 						<h3>LTL</h3>
@@ -131,80 +132,82 @@
 				</div>
 			{/if}
 		{/each}
-		{#each tableData as data}
-			{#if data.originCityName === selectedCity && data.originStateName === selectedState && data.loadID !== selectedRow}
-				{multiples = true}
-				<div class="size-80 top-0">
-					<DrawerMap
-						originLat={data.originLat}
-						originLng={data.originLng}
-						originCity={data.originCityName}
-						originState={data.originStateName}
-						destLat={data.destinationLat}
-						destLng={data.destinationLng}
-						destCity={data.destinationCityName}
-						destState={data.destinationStateName}
-					/>
-				</div>
-				<!-- load info -->
-				<div>
-					<h2>Origin Information</h2>
-					<div>
-						<h3>Load Date</h3>
-						<p>{data.loadID}</p>
+		{#if !tableClicked}
+			{#each tableData as data}
+				{#if data.originCityName === selectedCity && data.originStateName === selectedState && data.loadID !== selectedRow}
+					{(multiples = true)}
+					<div class="top-0 size-80">
+						<DrawerMap
+							originLat={data.originLat}
+							originLng={data.originLng}
+							originCity={data.originCityName}
+							originState={data.originStateName}
+							destLat={data.destinationLat}
+							destLng={data.destinationLng}
+							destCity={data.destinationCityName}
+							destState={data.destinationStateName}
+						/>
 					</div>
+					<!-- load info -->
+					<div>
+						<h2>Origin Information</h2>
+						<div>
+							<h3>Load Date</h3>
+							<p>{data.loadID}</p>
+						</div>
 
-					<div>
-						<h3>Origin</h3>
-						<p></p>
-					</div>
-					<div>
-						<h3>Origin Terminal</h3>
-						<p></p>
-					</div>
-					<h2>Destination Info</h2>
+						<div>
+							<h3>Origin</h3>
+							<p></p>
+						</div>
+						<div>
+							<h3>Origin Terminal</h3>
+							<p></p>
+						</div>
+						<h2>Destination Info</h2>
 
-					<div>
-						<h3>Delivery Date</h3>
-						<p>Delivery Destination</p>
-					</div>
-					<h2>Commodity Info</h2>
+						<div>
+							<h3>Delivery Date</h3>
+							<p>Delivery Destination</p>
+						</div>
+						<h2>Commodity Info</h2>
 
-					<div>
-						<h3>Dimensions</h3>
-						<p>Commodity</p>
+						<div>
+							<h3>Dimensions</h3>
+							<p>Commodity</p>
+						</div>
+						<div>
+							<h3>Pieces</h3>
+							<p></p>
+						</div>
+						<h2>Other Info</h2>
+						<div>
+							<h3>Agent</h3>
+							<p></p>
+						</div>
+						<div>
+							<h3>Phone Number</h3>
+							<p></p>
+						</div>
+						<div>
+							<h3>Miles</h3>
+							<p></p>
+						</div>
+						<div>
+							<h3>Revenue</h3>
+							<p></p>
+						</div>
+						<div>
+							<h3>LTL</h3>
+							<p></p>
+						</div>
+						<div>
+							<h3>Notes</h3>
+							<p></p>
+						</div>
 					</div>
-					<div>
-						<h3>Pieces</h3>
-						<p></p>
-					</div>
-					<h2>Other Info</h2>
-					<div>
-						<h3>Agent</h3>
-						<p></p>
-					</div>
-					<div>
-						<h3>Phone Number</h3>
-						<p></p>
-					</div>
-					<div>
-						<h3>Miles</h3>
-						<p></p>
-					</div>
-					<div>
-						<h3>Revenue</h3>
-						<p></p>
-					</div>
-					<div>
-						<h3>LTL</h3>
-						<p></p>
-					</div>
-					<div>
-						<h3>Notes</h3>
-						<p></p>
-					</div>
-				</div>
-			{/if}
-		{/each}
+				{/if}
+			{/each}
+		{/if}
 	</div>
 </Drawer>
