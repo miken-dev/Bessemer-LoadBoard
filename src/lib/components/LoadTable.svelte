80d031d5e5862216ddaad38785625321254c5f50
Michael Neal <mike@miken.dev>
2025-01-20 23:18:18 -0500

    remove unused sample data

--- src/lib/components/LoadTable.svelte
+++ src/lib/components/LoadTable.svelte
@@ -10,91 +10,9 @@
 		Modal
 	} from 'flowbite-svelte';
 	import { slide } from 'svelte/transition';
-	import LoadTableRow from './LoadTableRow.svelte';
-	const items = [
-		{
-			loadDate: '20250110T00:00:00',
-			OriginCityName: 'Cleveland',
-			OriginStateName: 'OH',
 
-			DeliveryDate: '20250110T00:00:00',
-			DestinationCityName: 'Akron',
-			DestinationStateName: 'OH',
 
-			LengthFeet: '52',
-			LengthInches: '5',
-			WidthFeet: '6',
-			WidthInches: '11',
-			HeightFeet: '9',
-			HeightInches: '9',
-			WeightInPounds: '65424',
-			Commodity: 'MACHINERY NOI/MISC. MILL',
 
-			Revenue: '8500',
-			Miles: '50',
-
-			Agent: 'John Smith',
-			AgentPhone: '(216) 555-1234'
-		},
-
-		{
-			loadDate: '20250110T00:00:00',
-			OriginCityName: 'Cleveland',
-			OriginStateName: 'OH',
-
-			DeliveryDate: '20250110T00:00:00',
-			DestinationCityName: 'Akron',
-			DestinationStateName: 'OH',
-
-			LengthFeet: '52',
-			LengthInches: '5',
-			WidthFeet: '6',
-			WidthInches: '11',
-			HeightFeet: '9',
-			HeightInches: '9',
-			WeightInPounds: '65424',
-			Commodity: 'MACHINERY NOI/MISC. MILL',
-
-			Revenue: '8500',
-			Miles: '50',
-
-			Agent: 'John Smith',
-			AgentPhone: '(216) 555-1234'
-		},
-		{
-			loadDate: '20250110T00:00:00',
-			OriginCityName: 'Cleveland',
-			OriginStateName: 'OH',
-
-			DeliveryDate: '20250110T00:00:00',
-			DestinationCityName: 'Akron',
-			DestinationStateName: 'OH',
-
-			LengthFeet: '52',
-			LengthInches: '5',
-			WidthFeet: '6',
-			WidthInches: '11',
-			HeightFeet: '9',
-			HeightInches: '9',
-			WeightInPounds: '65424',
-			Commodity: 'MACHINERY NOI/MISC. MILL',
-
-			Revenue: '8500',
-			Miles: '50',
-
-			Agent: 'John Smith',
-			AgentPhone: '(216) 555-1234'
-		}
-	];
-
-	let openRow;
-	let details;
-	let doubleClickModal = false;
-
-	const toggleRow = (i) => {
-		openRow = openRow === i ? null : i;
-	};
-</script>
 
 <Table>
 	<TableHead>
