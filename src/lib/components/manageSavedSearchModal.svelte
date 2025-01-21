8b570f0e86fde1965c837132f5a541f084854663
Michael Neal <mike@miken.dev>
2025-01-21 13:59:47 -0500

    create managed search modal

---
+++ src/lib/components/manageSavedSearchModal.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { Modal } from "flowbite-svelte";
+	import SavedSearchesRow from "./SavedSearchesRow.svelte";
+	let {
+		manageSavedSearchIsShowing = $bindable()
+	}: {
+		manageSavedSearchIsShowing: boolean
+	} = $props()
+</script>
+
+<Modal title="Manage Saved Searches" size="lg" bind:open={manageSavedSearchIsShowing}  outsideclose> 
+	
+	<SavedSearchesRow title="Saved Search #1" text={false} email={true} />
+	<SavedSearchesRow title="My saved search" text={true} email={false} />
+	<SavedSearchesRow title="Sample search #3" text={false} email={false} />
+	<SavedSearchesRow title="This is another search" text={true} email={true} />
+	<SavedSearchesRow title="Saved Search #1" text={false} email={true} />
+	<SavedSearchesRow title="My saved search" text={true} email={false} />
+	<SavedSearchesRow title="Sample search #3" text={false} email={false} />
+	<SavedSearchesRow title="This is another search" text={true} email={true} />
+	<SavedSearchesRow title="Saved Search #1" text={false} email={true} />
+	<SavedSearchesRow title="My saved search" text={true} email={false} />
+	<SavedSearchesRow title="Sample search #3" text={false} email={false} />
+	<SavedSearchesRow title="This is another search" text={true} email={true} />
+
+</Modal>
