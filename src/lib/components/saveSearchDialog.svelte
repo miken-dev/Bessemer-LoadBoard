29a50213fbdf9dd8ece62fdea59fe986e8ad2f7f
Michael Neal <mike@miken.dev>
2025-01-21 13:59:53 -0500

    create saved search dialog

---
+++ src/lib/components/saveSearchDialog.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { Button, Modal, Label, Input } from 'flowbite-svelte';
+
+	let {
+		saveSearchDialogIsShowing = $bindable(),
+		saved = $bindable()
+	}: {
+		saveSearchDialogIsShowing: boolean;
+		saved: boolean;
+	} = $props();
+</script>
+
+<Modal title="Name your saved search" size="md" bind:open={saveSearchDialogIsShowing} outsideclose>
+	<Label for="name" class="mb-2 block">Name your saved search</Label>
+	<Input id="name" placeholder="" />
+	<Button
+		size="md"
+		color="blue"
+		onclick={() => {
+			saved = true;
+		}}>Save</Button
+	>
+</Modal>
