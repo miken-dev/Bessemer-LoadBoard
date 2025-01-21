868f1131bef95cf729395f044aa129acbf58754f
Michael Neal <mike@miken.dev>
2025-01-21 13:58:43 -0500

    add logo, remove equipment button

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
 	import { DarkMode } from 'flowbite-svelte';
+	import  logo  from '$lib/assets/logo.png'
 </script>
 
-<div class="flex w-screen items-center justify-between bg-blue-600 p-5">
+<div class="flex w-screen items-center justify-between border-b-4 border-sky-400 p-5">
 	<div>
-		<img src="https://placehold.co/300x70/ff00ff/ffffff" />
+		<img src="{logo}" />
 	</div>
 	<div>
-		<button class="rounded bg-gray-500 px-4 py-2 font-bold text-white hover:bg-blue-600"
+		<!--	<button class="rounded bg-gray-500 px-4 py-2 font-bold text-white hover:bg-blue-600"
 			>Equipment Search</button
-		>
+		> -->
 		<DarkMode dark:text-gray-100 />
 	</div>
 </div>
