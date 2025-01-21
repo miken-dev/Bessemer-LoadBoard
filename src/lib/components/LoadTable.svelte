9dc245ec6fff48b72cb2eeabee5f8d323282a394
Michael Neal <mike@miken.dev>
2025-01-20 23:18:51 -0500

    add properties for updated layout

--- src/lib/components/LoadTable.svelte
+++ src/lib/components/LoadTable.svelte
@@ -11,8 +11,22 @@
 	} from 'flowbite-svelte';
 	import { slide } from 'svelte/transition';
 
+	import type LoadTableDetail from './LoadTableDetail.svelte';
+	import type { TableDataTypes } from '$lib/types';
 
 
+	// component props
+	let {
+		tabledata,
+		selectedrow = $bindable(),
+		detailshidden = $bindable(),
+		tableclicked = $bindable(),
+	}: {
+		tabledata: TableDataTypes[];
+		selectedrow: number | null;
+		detailshidden: boolean;
+		tableclicked: boolean;
+	} = $props();
 
 <Table>
 	<TableHead>
@@ -31,6 +45,23 @@
 						secondRow={`${item.OriginCityName}, ${item.OriginStateName}`}
 					/>
 				</TableBodyCell>
+	const month = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+	const day = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+	const dateordinals = (d: number) => {
+		if (d > 3 && d < 21) return 'th';
+		switch (d % 10) {
+			case 1: return "st";
+			case 2: return "nd";
+			case 3: return "rd";
+			default: return "th";
+		}
+	}
+	const toggle = (id: number) => {
+		selectedrow = id;
+		detailshidden = false 
+		tableclicked = true
+	}
+</script>
 
 				<TableBodyCell>
 					<LoadTableRow
