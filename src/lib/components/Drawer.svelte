28e9f4baee952b7f9a55bf9a6bdc6ec0a5847651
Michael Neal <mike@miken.dev>
2025-01-21 13:58:12 -0500

    fix data model calls

--- src/lib/components/Drawer.svelte
+++ src/lib/components/Drawer.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Drawer, Button, CloseButton } from 'flowbite-svelte';
+	import { Drawer, Hr, Button, CloseButton } from 'flowbite-svelte';
 	import { sineIn } from 'svelte/easing';
 	import type { TableDataTypes } from '$lib/types';
 	import SearchOptions from './SearchOptions.svelte';
@@ -72,17 +72,23 @@
 	let multiples = $state(false);
 </script>
 
-<Drawer transitionType="fly" {transitionParams} bind:hidden={detailsHidden} id="loadDetailDrawer">
-	<div class="flex flex-col items-center">
+<Drawer transitionType="fly" {transitionParams} bind:hidden={detailsHidden} class="w-100" id="loadDetailDrawer">
+	<CloseButton on:click={() => (detailsHidden = true)} class="mb-4 dark:text-white" />
+
+	<div class="flex flex-col justify-items-start">
 		<!-- Inline map -->
 		{#if multiples && !tableClicked}
-			<h2>Multiple available in {selectedCity} {selectedState}</h2>
+			<h2 class="text-l text-center font-extrabold">
+				Multiple loads available in <br />{selectedCity}, {selectedState}
+			</h2>
 		{/if}
 		{#each tableData as data}
 			{#if data.loadID == selectedRow}
-				{(selectedCity = data.originCityName)}
-				{(selectedState = data.originStateName)}
-				<div class="top-0 size-80">
+				<div class="hidden">
+					{(selectedCity = data.originCityName)}
+					{(selectedState = data.originStateName)}
+				</div>
+				<div class="top-0 size-80 ">
 					<DrawerMap
 						originLat={data.originLat}
 						originLng={data.originLng}
@@ -95,68 +101,75 @@
 					/>
 				</div>
 				<!-- load info -->
-				<div>
+				<div class="ml-3">
+					<!---
 					{#each columns as column}
-						<div class="flex flex-row">
+						<div class="flex flex-row mt-5">
 							<h3>{column.title}</h3>
 							<p>{data[column.property]}</p>
 						</div>
-					{/each}
-					<div class="flex flex-row">
-						<h3>Load Date:</h3>
-						<p>
-							{` ${day[new Date(data.loadDate).getDay()]} ${month[new Date(data.loadDate).getMonth()]} ${new Date(data.loadDate).getFullYear()}`}
-						</p>
+					{/each} -->
+					<div class="flex flex-col my-1 mt-4 ">
+						<h3 class="font-extrabold">Load Date:</h3>
+						<p>	{`${day[new Date(data.loadDate).getDay()]} ${month[new Date(data.loadDate).getMonth()]} ${new Date(data.loadDate).getDate()}${dateOrdinals(new Date(data.loadDate).getDate())} ${new Date(data.loadDate).getFullYear()}`}</p>
 					</div>
 
-					<div class="flex">
-						<h3>Origin:</h3>
+					<div class="flex flex-col my-1 mt-3">
+						<h3 class="font-extrabold">Origin:</h3>
 						<p>{` ${data.originCityName}, ${data.originStateName}`}</p>
 					</div>
-					<div class="flex">
-						<h3>Origin Terminal:</h3>
-						<p></p>
+					<div class="flex flex-col my-1 mt-3">
+						<h3 class="font-extrabold">Origin Terminal:</h3>
+						<p>{`${data.terminalName}`}</p>
 					</div>
-					<h2>Destination Info</h2>
+					<div class="flex flex-col my-1 mt-3">
+						<h3 class="font-extrabold">Delivery Date:</h3>
+						<p>{`${day[new Date(data.deliveryDate).getDay()]} ${month[new Date(data.deliveryDate).getMonth()]} ${new Date(data.deliveryDate).getDate()}${dateOrdinals(new Date(data.deliveryDate).getDate())} ${new Date(data.deliveryDate).getFullYear()}`}</p>
+					</div>
+					<div class="flex flex-col my-1 mt-3">
+						<h3 class="font-extrabold">Delivery Destination:</h3>
+						<p>{`${data.destinationCityName}, ${data.destinationStateName}`}</p>
+					</div>
+					<div class="flex flex-col my-1 mt-3">
+						<h3 class="font-extrabold">Dimensions:</h3>
+						<p>{`${data.lengthFeet}'${data.lengthInches}" x ${data.widthFeet}'${data.widthInches}" x ${data.heightFeet}'${data.heightInches}"`}</p>
+					</div>
+					<div class="flex flex-col my-1 mt-3">
+						<h3 class="font-extrabold">Weight:</h3>
+						<p>{`${data.weightInPounds}lbs.`}</p>
+					</div>
+					<div class="flex flex-col my-1 mt-3">
+						<h3 class="font-extrabold">Commodity:</h3>
+						<p>{data.commodity}</p>
+					</div>
 
-					<div class="flex">
-						<h3>Delivery Date</h3>
-						<p>Delivery Destination</p>
+					<div class="flex flex-col my-1 mt-3">
+						<h3 class="font-extrabold">Number of Pieces:</h3>
+						<p>{data.pieceCount}</p>
 					</div>
-					<h2>Commodity Info</h2>
-
-					<div class="flex">
-						<h3>Dimensions</h3>
-						<p>Commodity</p>
+					<div class="flex flex-col my-1 mt-3">
+						<h3 class="font-extrabold">Agent:</h3>
+						<p>AGENT NAME</p>
 					</div>
-					<div class="flex">
-						<h3>Pieces</h3>
-						<p></p>
+					<div class="flex flex-col my-1 mt-3">
+						<h3 class="font-extrabold">Phone Number:</h3>
+						<p>{data.terminalPhone}</p>
 					</div>
-					<h2>Other Info</h2>
-					<div class="flex">
-						<h3>Agent</h3>
-						<p></p>
+					<div class="flex flex-col my-1 mt-3">
+						<h3 class="font-extrabold">Revenue:</h3>
+						<p>{`$${data.revenue}`}</p>
 					</div>
-					<div class="flex">
-						<h3>Phone Number</h3>
-						<p></p>
+					<div class="flex flex-col my-1 mt-3">
+						<h3 class="font-extrabold">Miles:</h3>
+						<p>{data.miles}</p>
 					</div>
-					<div class="flex">
-						<h3>Miles</h3>
-						<p></p>
+					<div class="flex flex-col my-1 mt-3">
+						<h3 class="font-extrabold">LTL:</h3>
+						<p>{data.ltl}</p>
 					</div>
-					<div class="flex">
-						<h3>Revenue</h3>
-						<p></p>
-					</div>
-					<div class="flex">
-						<h3>LTL</h3>
-						<p></p>
-					</div>
-					<div class="flex">
-						<h3>Notes</h3>
-						<p></p>
+					<div class="flex flex-col my-1 mt-3 max-w-80">
+						<h3 class="font-extrabold">Notes:</h3>
+						<p>{data.notes}</p>
 					</div>
 				</div>
 			{/if}
@@ -164,7 +177,8 @@
 		{#if !tableClicked}
 			{#each tableData as data}
 				{#if data.originCityName === selectedCity && data.originStateName === selectedState && data.loadID !== selectedRow}
-					{(multiples = true)}
+					<div class="hidden">{(multiples = true)}</div>
+					<Hr classHr="my-8 h-1 w-auto bg-slate-500 dark:bg-slate-200  rounded" />
 					<div class="top-0 size-80">
 						<DrawerMap
 							originLat={data.originLat}
@@ -179,63 +193,69 @@
 					</div>
 					<!-- load info -->
 					<div>
-						<div class="flex flex-row">
-							<h3>Load Date:</h3>
-							<p>
-								{`${day[new Date(data.loadDate).getDay()]} ${month[new Date(data.loadDate).getMonth()]} ${new Date(data.loadDate).getFullYear()}`}
-							</p>
+						<div class="flex flex-col my-1 mt-3 ">
+							<h3 class="font-extrabold">Load Date:</h3>
+							<p>	{`${day[new Date(data.loadDate).getDay()]} ${month[new Date(data.loadDate).getMonth()]} ${new Date(data.loadDate).getDate()}${dateOrdinals(new Date(data.loadDate).getDate())} ${new Date(data.loadDate).getFullYear()}`}</p>
 						</div>
-
-						<div class="flex">
-							<h3>Origin:</h3>
-							<p>{`${data.originCityName}, ${data.originStateName}`}</p>
+	
+						<div class="flex flex-col my-1 mt-3">
+							<h3 class="font-extrabold">Origin:</h3>
+							<p>{` ${data.originCityName}, ${data.originStateName}`}</p>
 						</div>
-						<div class="flex">
-							<h3>Origin Terminal:</h3>
-							<p></p>
+						<div class="flex flex-col my-1 mt-3">
+							<h3 class="font-extrabold">Origin Terminal:</h3>
+							<p>{`${data.terminalName}`}</p>
 						</div>
-						<h2>Destination Info</h2>
-
-						<div class="flex">
-							<h3>Delivery Date</h3>
-							<p>Delivery Destination</p>
+	
+						<div class="flex flex-col my-1 mt-3">
+							<h3 class="font-extrabold">Delivery Date:</h3>
+							<p>{`${day[new Date(data.deliveryDate).getDay()]} ${month[new Date(data.deliveryDate).getMonth()]} ${new Date(data.deliveryDate).getDate()}${dateOrdinals(new Date(data.deliveryDate).getDate())} ${new Date(data.deliveryDate).getFullYear()}`}</p>
 						</div>
-						<h2>Commodity Info</h2>
-
-						<div class="flex">
-							<h3>Dimensions</h3>
-							<p>Commodity</p>
+						<div class="flex flex-col my-1 mt-3">
+							<h3 class="font-extrabold">Delivery Destination:</h3>
+							<p>{`${data.destinationCityName}, ${data.destinationStateName}`}</p>
 						</div>
-						<div class="flex">
-							<h3>Pieces</h3>
-							<p></p>
+						<div class="flex flex-col my-1 mt-3">
+							<h3 class="font-extrabold">Dimensions:</h3>
+							<p>{`${data.lengthFeet}'${data.lengthInches}" x ${data.widthFeet}'${data.widthInches}" x ${data.heightFeet}'${data.heightInches}" x `}</p>
 						</div>
-						<h2>Other Info</h2>
-						<div class="flex">
-							<h3>Agent</h3>
-							<p></p>
+						<div class="flex flex-col my-1 mt-3">
+							<h3 class="font-extrabold">Weight:</h3>
+							<p>{`${data.weightInPounds}lbs.`}</p>
 						</div>
-						<div class="flex">
-							<h3>Phone Number</h3>
-							<p></p>
+						<div class="flex flex-col my-1 mt-3">
+							<h3 class="font-extrabold">Commodity:</h3>
+							<p>{data.commodity}</p>
 						</div>
-						<div class="flex">
-							<h3>Miles</h3>
-							<p></p>
+	
+						<div class="flex flex-col my-1 mt-3">
+							<h3 class="font-extrabold">Number of Pieces:</h3>
+							<p>{data.pieceCount}</p>
 						</div>
-						<div class="flex">
-							<h3>Revenue</h3>
-							<p></p>
+						<div class="flex flex-col my-1 mt-3">
+							<h3 class="font-extrabold">Agent:</h3>
+							<p>AGENT NAME</p>
 						</div>
-						<div class="flex">
-							<h3>LTL</h3>
-							<p></p>
+						<div class="flex flex-col my-1 mt-3">
+							<h3 class="font-extrabold">Phone Number:</h3>
+							<p>{data.terminalPhone}</p>
 						</div>
-						<div class="flex">
-							<h3>Notes</h3>
-							<p></p>
+						<div class="flex flex-col my-1 mt-3">
+							<h3 class="font-extrabold">Revenue:</h3>
+							<p>{`$${data.revenue}`}</p>
 						</div>
-					</div>
+						<div class="flex flex-col my-1 mt-3">
+							<h3 class="font-extrabold">Miles:</h3>
+							<p>{data.miles}</p>
+						</div>
+						<div class="flex flex-col my-1 mt-3">
+							<h3 class="font-extrabold">LTL:</h3>
+							<p>{data.ltl}</p>
+						</div>
+						<div class="flex flex-col my-1 mt-3 w-80">
+							<h3 class="font-extrabold">Notes:</h3>
+							<p>{data.notes}</p>
+						</div>					</div>
 				{/if}
 			{/each}
 		{/if}
