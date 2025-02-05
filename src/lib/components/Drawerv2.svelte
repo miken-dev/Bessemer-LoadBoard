f0dc3c5dbe3c53f043733914f33359556d5c986e
Michael Neal <mike@miken.dev>
2025-02-05 00:05:23 -0500

    add new city prop, rework carousel

--- src/lib/components/Drawerv2.svelte
+++ src/lib/components/Drawerv2.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
 	import DrawerDetails from './DrawerDetails.svelte';
 	import DrawerCarousel from './DrawerCarousel.svelte';
+	import { MediaQuery } from 'svelte/reactivity';
 
 	import { Drawer, Hr, Button, CloseButton } from 'flowbite-svelte';
 	import { sineIn } from 'svelte/easing';
 	import type { TableDataTypes } from '$lib/types';
 	let {
 		tableData,
+		selectedCity,
 		selectedRow = $bindable(),
 		detailsHidden = $bindable(),
 		tableClicked
 	}: {
-		tableData: TableDataTypes[];
+		tableData: any;
+		selectedCity: string | null;
 		selectedRow: number | null;
 		detailsHidden: boolean;
 		tableClicked: boolean;
@@ -19,39 +22,105 @@
 
 	let isSingleRow = () => {
 		tableData.length === 1;
-	}
+	};
 	let transitionParams = {
 		x: -320,
 		duration: 200,
 		easing: sineIn
 	};
-	let selectedCity = $state('');
-	let selectedState = $state('');
 
+	let transitionParamsDesktop = {
+		y: 320,
+		duration: 200,
+		easing: sineIn
+	};
 	function singleItem(data: any) {
-		if(data.length === 1) {
-			return true
-		} 
-		return false
+		if (data.length === 1) {
+			return true;
+		}
+		return false;
 	}
 
+	const desktop = new MediaQuery('min-width: 600px');
 </script>
 
-<Drawer
-	transitionType="fly"
-	{transitionParams}
-	bind:hidden={detailsHidden}
-	class="w-100"
-	id="loadDetailsDrawerMobile"
->
-	<CloseButton on:click={() => (detailsHidden = true)} class="mb-4, dark:text-white" />
-	{#if tableClicked || singleItem(tableData)}
-		<DrawerDetails data={tableData[0]} />
-	{:else}
-		<DrawerCarousel items={tableData}>
-			{#snippet content({ tableData })}
-				<DrawerDetails {...tableData} /> 
-			{/snippet}
-		</DrawerCarousel>
-	{/if}
-</Drawer>
+{#if !desktop.current}
+	<Drawer
+		transitionType="fly"
+		{transitionParams}
+		bind:hidden={detailsHidden}
+		class="w-100 mobile"
+		id="loadDetailsDrawerMobile"
+	>
+		<CloseButton on:click={() => (detailsHidden = true)} class="mb-4, dark:text-white" />
+		{#if tableClicked || singleItem(tableData)}
+			{#each tableData as data}
+				{#if data.loadID === selectedRow}
+					<DrawerDetails {data} horizontal={false} />
+				{/if}
+			{/each}
+		{:else}
+			<div class="container">
+				<p>Multiple loads available in <strong>{selectedCity}</strong></p>
+				<DrawerCarousel>
+					{#each tableData as data}
+						{#if data.originCityName === selectedCity}
+							{#if data.loadID === selectedRow}
+								<DrawerDetails {data} horizontal={false} />
+							{/if}
+						{/if}
+					{/each}
+				</DrawerCarousel>
+			</div>
+		{/if}
+	</Drawer>
+{/if}
+{#if desktop.current}
+	<Drawer
+		placement="bottom"
+		transitionType="fly"
+		transitionParams={transitionParamsDesktop}
+		bind:hidden={detailsHidden}
+		class="w-100 desktop"
+		id="loadDetailsDrawerDesktop"
+	>
+		<CloseButton on:click={() => (detailsHidden = true)} class="mb-4, dark:text-white" />
+
+		<div class="container">
+			{#if tableClicked || singleItem(tableData)}
+				{#each tableData as data}
+					{#if data.loadID === selectedRow}
+						<DrawerDetails {data} horizontal={true} />
+					{/if}
+				{/each}
+			{:else}
+				<p class="text-center text-2xl">
+					Multiple loads available in <strong>{selectedCity}</strong>
+				</p>
+				<br />
+				<DrawerCarousel>
+					{#each tableData as data}
+						{#if data.originCityName === selectedCity}
+							<div class="w-dvw">
+								<DrawerDetails {data} horizontal={true} />
+							</div>
+						{/if}
+					{/each}
+				</DrawerCarousel>
+			{/if}
+		</div>
+	</Drawer>
+{/if}
+
+<style>
+	@media screen and (min-width: 601px) {
+		.container {
+			width: dvw;
+		}
+	}
+	@media screen and (max-width: 600px) {
+		.container {
+			width: 390px;
+		}
+	}
+</style>
