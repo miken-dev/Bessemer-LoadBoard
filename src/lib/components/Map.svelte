0d32b54b1dcc79438c1e682c56d838a67e404e40
Michael Neal <mike@miken.dev>
2025-02-10 23:59:57 -0500

    add browser window checks

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { Map, TileLayer, Marker } from 'sveaflet';
-
+	import { browser } from '$app/environment';
 	import type { TableDataTypes } from '$lib/types';
 
 	let {
@@ -24,24 +24,29 @@
 	const toggle = (id: number, city: string) => {
 		selectedRow = id;
 		selectedCity = city;
-		console.log(city)
-		console.log(selectedCity)
-		console.log(city === selectedCity)
+		console.log(city);
+		console.log(selectedCity);
+		console.log(city === selectedCity);
 		detailsHidden = false;
 		tableClicked = false;
 	};
 </script>
 
-<div class="h-full w-full md:mt-20">
-	<Map
-		options={{
-			center: [44.67498, -103.856037],
-			zoom: 4.4
-		}}
-	>
-		<TileLayer url={'https://tile.openstreetmap.org/{z}/{x}/{y}.png'} />
-		{#each tableData as row}
-			<Marker onclick={() => toggle(row.loadID, row.originCityName)} latLng={[row.originLat, row.originLng]} />
-		{/each}
-	</Map>
-</div>
+{#if browser}
+	<div class="h-full w-full md:mt-20">
+		<Map
+			options={{
+				center: [44.67498, -103.856037],
+				zoom: 4.4
+			}}
+		>
+			<TileLayer url={'https://tile.openstreetmap.org/{z}/{x}/{y}.png'} />
+			{#each tableData as row}
+				<Marker
+					onclick={() => toggle(row.loadID, row.originCityName)}
+					latLng={[row.originLat, row.originLng]}
+				/>
+			{/each}
+		</Map>
+	</div>
+{/if}
