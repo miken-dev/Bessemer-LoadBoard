535e769b66ab4a4dd893584b463ed776e5ce4f07
Michael Neal <mike@miken.dev>
2025-02-11 01:10:43 -0500

    import pocketbase

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,7 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+import type PocketBase, { AuthModel } from 'pocketbase';
+
 declare global {
 	namespace App {
 		// interface Error {}
@@ -7,6 +9,10 @@ declare global {
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
+		interface Locals {
+			pb: PocketBase;
+			user: AuthModel | null;
+		}
 	}
 }
 declare module leaflet { }
