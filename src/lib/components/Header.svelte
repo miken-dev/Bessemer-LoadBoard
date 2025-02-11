72dfa6ae64d2ef937c3517593af50297fb6fce36
Michael Neal <mike@miken.dev>
2025-02-10 23:59:40 -0500

    add login button

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,16 +1,32 @@
 <script lang="ts">
-	import { DarkMode } from 'flowbite-svelte';
-	import  logo  from '$lib/assets/logo.png'
+	import { DarkMode, ButtonGroup, Button } from 'flowbite-svelte';
+	import logo from '$lib/assets/logo.png';
+	let {
+		loggedIn = $bindable()
+	}: {
+		loggedIn: Boolean;
+	} = $props();
+
+	let logout = () => {
+		loggedIn = false
+		console.log("Logged out")
+		// TODO: add logout logic
+	}
 </script>
 
 <div class="flex w-screen items-center justify-between border-b-4 border-sky-400 p-5">
 	<div>
-		<img src="{logo}" />
+		<img src={logo} />
 	</div>
 	<div>
 		<!--	<button class="rounded bg-gray-500 px-4 py-2 font-bold text-white hover:bg-blue-600"
 			>Equipment Search</button
 		> -->
-		<DarkMode class="dark:text-gray-100" />
+		<DarkMode class="dark:text-gray-100 pt-2" />
+		{#if loggedIn}
+			<Button on:click={logout} color="blue">Log Out</Button>
+		{:else}
+			<Button href="https://operator.ddsexpress.com/user-login/" color="blue">Log In</Button>
+		{/if}
 	</div>
 </div>
