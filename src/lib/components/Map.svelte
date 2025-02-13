6ac8abd0643d3769fe8d2961b977975c1ea556df
Michael Neal <mike@miken.dev>
2025-02-13 02:30:57 -0500

    add marker for when there's multiple loads in the same spot

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { Map, TileLayer, Marker } from 'sveaflet';
+	import { Map, TileLayer, Marker, Popup } from 'sveaflet';
 	import { browser } from '$app/environment';
 	import type { TableDataTypes } from '$lib/types';
 
@@ -33,7 +34,7 @@
 </script>
 
 {#if browser}
-	<div class="h-full w-full md:mt-20">
+	<div class="mt-8 h-full w-full">
 		<Map
 			options={{
 				center: [44.67498, -103.856037],
@@ -42,10 +43,24 @@
 		>
 			<TileLayer url={'https://tile.openstreetmap.org/{z}/{x}/{y}.png'} />
 			{#each tableData as row}
-				<Marker
-					onclick={() => toggle(row.loadID, row.originCityName)}
-					latLng={[row.originLat, row.originLng]}
-				/>
+				{#if row.areaLoadCount === 1}
+					<Marker
+						onclick={() => toggle(row.originCityName, row.loadID)}
+						latLng={[row.originLat, row.originLng]}
+					/>
+				{:else}
+					<Popup
+						onclick={() => toggle(row.originCityName)}
+						latLng={[row.originLat, row.originLng]}
+						options={{
+							autoClose: false,
+							closeOnEscapeKey: false,
+							closeOnClick: false
+						}}
+					>
+						<div class="text-2xl font-bold text-center">{row.areaLoadCount}</div>
+					</Popup>
+				{/if}
 			{/each}
 		</Map>
 	</div>
