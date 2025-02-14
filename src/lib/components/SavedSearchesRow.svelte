3f171bf294ce3a017f3daa42307da9bb59f53f7f
Michael Neal <mike@miken.dev>
2025-02-14 04:12:13 -0500

    added props and markup to handle filtering

--- src/lib/components/SavedSearchesRow.svelte
+++ src/lib/components/SavedSearchesRow.svelte
@@ -1,14 +1,29 @@
 <script lang="ts">
+	type setFilterProps = {
+		setFilters: (
+			originMiles: number,
+			originState: string,
+			originCity: string,
+			destMiles: number,
+			destState: string,
+			destCity: string,
+			trailerTypes: string,
+			fromDate: Date,
+			toDate: Date
+		) => void;
+	};
 	let active = 'bg-blue-600';
 	let inactive = 'bg-gray-500';
 	let {
 		title = $bindable(),
 		text = $bindable(),
-		email = $bindable()
+		email = $bindable(),
+		setFilters
 	}: {
 		title: string;
 		text: boolean;
 		email: boolean;
+		setFilters: setFilterProps["setFilters"];
 	} = $props();
 </script>
 
@@ -16,9 +31,10 @@
 	<p>{title}</p>
 	<p></p>
 	<div class="flex flex-row justify-around">
-		<button class="{text ? active : inactive} text-white py-2 px-4 my-3 mx-1 rounded">Text</button>
-		<button class="{email ? active : inactive} text-white mx-1 my-3 rounded px-4 py-2">Email</button>
-		<button class="bg-blue-600 mx-1 my-3 rounded px-4 py-2 text-white">View</button>
-		<button class="bg-blue-600 mx-1 my-3 rounded px-4 py-2 text-white">Delete</button>
+		<button class="{text ? active : inactive} mx-1 my-3 rounded px-4 py-2 text-white">Text</button>
+		<button class="{email ? active : inactive} mx-1 my-3 rounded px-4 py-2 text-white">Email</button
+		>
+		<button onclick={setFilters} class="mx-1 my-3 rounded bg-blue-600 px-4 py-2 text-white">View</button>
+		<button class="mx-1 my-3 rounded bg-blue-600 px-4 py-2 text-white">Delete</button>
 	</div>
 </div>
