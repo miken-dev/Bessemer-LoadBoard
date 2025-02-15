e91bd8e8e1165d38a91527c5ab2ccfbdf6cf9b79
Michael Neal <mike@miken.dev>
2025-02-15 16:04:32 -0500

    refactor to encapsulate components into larger components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,15 +12,15 @@
 	import { writable, type Writable } from 'svelte/store';
 
 	// Component imports
-	import DataTable from '$lib/components/LoadTablev2.svelte';
-	import Drawerv2 from '$lib/components/Drawerv2.svelte';
 	import NewSearch from '$lib/components/NewSearch.svelte';
-	import ViewsBar from '$lib/components/ViewsBar.svelte';
 	import Header from '$lib/components/Header.svelte';
-	import Map from '$lib/components/Map.svelte';
 	import ManageSavedSearchModal from '$lib/components/manageSavedSearchModal.svelte';
 	import SavedSearches from '$lib/components/SavedSearches.svelte';
 	import SaveSearchDialog from '$lib/components/saveSearchDialog.svelte';
+	import PublicData from '$lib/components/PublicData.svelte';
+	import LoggedInData from '$lib/components/LoggedInData.svelte';
+	import NotificationPreferencesModal from '$lib/components/NotificationPreferencesModal.svelte';
+	import SearchOptions from '$lib/components/SearchOptions.svelte';
 
 	// Application State
 	let selectedRow: number | null = $state(null);
@@ -34,8 +34,7 @@
 	let saveSearchDialogIsShowing: boolean = $state(false);
 	let manageSavedSearchIsShowing: boolean = $state(false);
 	let searchOptionsIsShowing: boolean = $state(true);
-	let tableIsShowing: boolean = $state(true);
-	let mapIsShowing: boolean = $state(true);
+	let NotificationPreferencesShowing: boolean = $state(false);
 
 	// filters
 
@@ -50,111 +49,6 @@
 	let toDateRange: Date | undefined = $state(undefined);
 	let filter: string = $state('isPublic = "true"');
 	// PocketBase
-	const PB = new PocketBase('https://bessemer-loadboard.pockethost.io');
-
-	let tableData: [TableDataTypes] | [] = $state([]);
-
-	async function getRecords() {
-		let records;
-		if (loggedIn) {
-			records = await PB.collection('Active_Loads').getFullList({});
-			const results: [TableDataTypes] = records.map((record) => {
-				return {
-					loadID: record.id,
-					loadDate: record.loadDate,
-					deliveryDate: record.deliveryDate,
-					originAddress: record.originAddress,
-					originZipCode: record.originZipCode,
-					destinationAddress: record.destinationAddress,
-					destinationZipCode: record.destinationZipCode,
-					commodity: record.commodity,
-					lengthFeet: record.lengthFeet,
-					lengthInches: record.lengthInches,
-					widthFeet: record.widthFeet,
-					widthInches: record.widthInches,
-					heightFeet: record.heightFeet,
-					heightInches: record.heightInches,
-					weightInPounds: record.weightInPounds,
-					pieceCount: record.pieceCount,
-					revenue: record.revenue,
-					notes: record.notes,
-					terminalID: record.terminalID,
-					terminalPhone: record.terminalPhone,
-					ltl: record.ltl,
-					isPublic: record.isPublic,
-					trailerTypes: record.trailerTypes,
-					miles: record.miles,
-					terminalName: record.terminalName,
-					originStateName: record.originStateName,
-					originCityName: record.originCityName,
-					originLat: record.originLat,
-					originLng: record.originLng,
-					destinationStateName: record.destinationStateName,
-					destinationCityName: record.destinationCityName,
-					destinationLat: record.destinationLat,
-					destinationLng: record.destinationLng,
-					areaLoadCount: record.areaLoadCount
-				};
-			});
-			return results;
-		} else {
-			records = await PB.collection('Active_Loads_Public').getFullList({});
-			const results: [TableDataTypes] = records.map((record) => {
-				return {
-					loadID: record.id,
-					loadDate: record.loadDate,
-					deliveryDate: record.deliveryDate,
-					originAddress: record.originAddress,
-					originZipCode: record.originZipCode,
-					destinationAddress: record.destinationAddress,
-					destinationZipCode: record.destinationZipCode,
-					commodity: record.commodity,
-					lengthFeet: record.lengthFeet,
-					lengthInches: record.lengthInches,
-					widthFeet: record.widthFeet,
-					widthInches: record.widthInches,
-					heightFeet: record.heightFeet,
-					heightInches: record.heightInches,
-					weightInPounds: record.weightInPounds,
-					pieceCount: record.pieceCount,
-					revenue: record.revenue,
-					notes: record.notes,
-					terminalID: record.terminalID,
-					terminalPhone: record.terminalPhone,
-					ltl: record.ltl,
-					isPublic: record.isPublic,
-					trailerTypes: record.trailerTypes,
-					miles: record.miles,
-					terminalName: record.terminalName,
-					originStateName: record.originStateName,
-					originCityName: record.originCityName,
-					originLat: record.originLat,
-					originLng: record.originLng,
-					destinationStateName: record.destinationStateName,
-					destinationCityName: record.destinationCityName,
-					destinationLat: record.destinationLat,
-					destinationLng: record.destinationLng,
-					areaLoadCount: record.areaLoadCount
-				};
-			});
-			return results;
-		}
-	}
-	PB.collection('Active_Loads_Public').subscribe('*', async (e) => {
-		tableData = await getRecords();
-	});
-	PB.collection('Active_Loads').subscribe('*', async (e) => {
-		tableData = await getRecords();
-	});
-
-	onMount(async () => {
-		tableData = await getRecords();
-	});
-
-	onDestroy(() => {
-		PB.collection('Active_Loads').unsubscribe('*');
-		PB.collection('Active_Loads_Public').unsubscribe('*');
-	});
 
 	// Cookie set up
 	function getUserId(): string | null {
@@ -178,11 +72,9 @@
 				userId = currentId; // Direct assignment
 				loggedIn = true;
 				filter = '';
-				getRecords();
 			} else {
 				loggedIn = false;
 				filter = 'isPublic="True"';
-				getRecords();
 			}
 		}, 1000);
 
@@ -224,22 +116,8 @@
 		return distanceMiles;
 	}
 
-	let mapWidth = (): string => {
-		if (tableIsShowing) {
-			return 'w-1/3';
-		} else {
-			return 'w-screen';
-		}
-	};
 	const toggleDetails = (id: string) => {
 		selectedRow = selectedRow === id ? null : id;
-	};
-	let tableWidth = (): string => {
-		if (tableIsShowing) {
-			return 'w-1/2';
-		} else {
-			return 'w-max m-auto';
-		}
 	};
 </script>
 
@@ -250,7 +128,7 @@
 				<strong>DEVELOPER MODE</strong> detailsHidden={detailsHidden}, selectedLoadID={selectedRow
 					? selectedRow
 					: 'null'}, tableClicked={tableClicked}, city={selectedCity}, userID={userId} multi={multipleLoads},
-				loggedIn={loggedIn}, tableLength = {tableData.length}
+				loggedIn={loggedIn} 
 			</p>
 		</div>
 	{/if}
@@ -270,79 +148,16 @@
 			<ChevronDownOutline class="ms-2  h-6 w-6 text-gray-800 dark:text-white" />
 		</Button>
 		{#if searchOptionsIsShowing}
-			<div class="flex flex-col md:flex-row">
-				<NewSearch
-					bind:originMilesFilter
-					bind:originStateFilter
-					bind:originCityFilter
-					bind:destMilesFilter
-					bind:destCityFilter
-					bind:destStateFilter
-					bind:trailerTypesFilter
-					bind:fromDateRange
-					bind:toDateRange
-					bind:saveSearchDialogIsShowing
-				/>
-				{#key manageSavedSearchIsShowing}
-					<SavedSearches
-						bind:originMilesFilter
-						bind:originStateFilter
-						bind:originCityFilter
-						bind:destMilesFilter
-						bind:destCityFilter
-						bind:destStateFilter
-						bind:trailerTypesFilter
-						bind:fromDateRange
-						bind:toDateRange
-						bind:manageSavedSearchIsShowing
-						{userId}
-					/>
-				{/key}
-			</div>
-		{/if}
+		<SearchOptions {originMilesFilter} {originStateFilter} {originCityFilter} {destMilesFilter} {destCityFilter} {destStateFilter} {trailerTypesFilter} {fromDateRange} {toDateRange} {saveSearchDialogIsShowing} {manageSavedSearchIsShowing} {userId} />
 	{/if}
-
-	<ViewsBar bind:tableIsShowing bind:mapIsShowing />
-
+	{/if}
+	
 	<!-- Table and Map -->
-	<div class="flex flex-col items-start justify-center md:flex-row">
-		{#if tableIsShowing}
-			<div class="min-h-screen {tableWidth} p-4 dark:bg-gray-800 dark:text-gray-100 md:p-8">
-				<div class="mx-auto max-w-[95rem]">
-					{#key tableData}
-						<DataTable
-							{tableData}
-							bind:selectedRow
-							bind:detailsHidden
-							bind:tableClicked
-							{loggedIn}
-						/>
-					{/key}
-				</div>
-			</div>
-		{/if}
-
-		{#if mapIsShowing}
-			<div class="sticky top-0 w-11/12 md:{mapWidth} h-lvh md:h-[35rem] lg:h-[50rem]">
-				<Map
-					bind:multipleLoads
-					{tableData}
-					bind:selectedCity
-					bind:selectedRow
-					bind:detailsHidden
-					bind:tableClicked
-				/>
-			</div>
-		{/if}
-	</div>
-	<Drawerv2
-		{tableData}
-		{selectedCity}
-		bind:selectedRow
-		bind:detailsHidden
-		{tableClicked}
-		{multipleLoads}
-	/>
+	{#if loggedIn}
+	<LoggedInData />
+	{:else}
+	<PublicData />
+	{/if}
 	<ManageSavedSearchModal bind:manageSavedSearchIsShowing />
 	<SaveSearchDialog
 		bind:saveSearchDialogIsShowing
@@ -358,4 +173,5 @@
 		{toDateRange}
 		{userId}
 	/>
+	<NotificationPreferencesModal {NotificationPreferencesShowing} {userId} />
 </main>
