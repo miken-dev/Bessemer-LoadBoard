93ff8954465c326646b4cc632854d097671e3f70
Michael Neal <mike@miken.dev>
2025-02-16 16:38:55 -0500

    refactor views to clean up hierarchy

--- src/lib/components/SavedSearches.svelte
+++ src/lib/components/SavedSearches.svelte
@@ -4,7 +4,7 @@
 	import { Button } from 'flowbite-svelte';
 	import type { savedSearchesTypes } from '$lib/types';
 	import SavedSearchesRow from './SavedSearchesRow.svelte';
-	let savedSearches: [savedSearchesTypes] | [] = $state([]);
+	import DeleteConfirmationModal from './DeleteConfirmationModal.svelte';
 
 	let {
 		//	originLatFilter = $bindable(),
@@ -21,6 +21,7 @@
 		fromDateRange = $bindable(),
 		toDateRange = $bindable(),
 		manageSavedSearchIsShowing = $bindable(),
+		savedSearches = $bindable(),
 		userId
 	}: {
 		//	originLatFilter: number | undefined;
@@ -38,17 +39,21 @@
 		toDateRange: Date | null | undefined;
 		manageSavedSearchIsShowing: boolean;
 		userId: string | null;
+		savedSearches: [savedSearchesTypes] | [];
 	} = $props();
 
 	let active = 'bg-blue-600';
 	let inactive = 'bg-gray-500';
+	let deleteConfirmationShowing = $state(false);
+	let deleteID = $state('');
+	let deleteName = $state('');
 
 	const PB = new PocketBase('https://bessemer-loadboard.pockethost.io');
 	async function getRecords() {
 		const records = await PB.collection('Saved_Searches').getFullList({
 			filter: `userID = "${userId}"`
 		});
-		const results: [savedSearchesTypes] = records.map((record) => {
+		const results: [savedSearchesTypes]  = records.map((record) => {
 			return {
 				id: record.id,
 				name: record.name,
@@ -68,9 +73,30 @@
 		return results;
 	}
 
-	onMount(async () => {
+	async function toggleEmail(currentValue: boolean, id: string) {
+		const record = await PB.collection('Saved_Searches').update(`${id}`, {
+			emailNotification: `${currentValue ? 'False' : 'True'}`
+		});
+
 		savedSearches = await getRecords();
-	});
+	}
+
+	async function toggleText(currentValue: boolean, id: string) {
+		const record = await PB.collection('Saved_Searches').update(`${id}`, {
+			textNotification: `${currentValue ? 'False' : 'True'}`
+		});
+
+		savedSearches = await getRecords();
+	}
+	function deleteRow(id: string, name: string) {
+		deleteConfirmationShowing = true;
+		deleteID = id;
+		deleteName = name;
+		console.log(deleteName)
+	}
+	async function refresh() {
+		savedSearches = await getRecords();
+	}
 
 	function setFilters(
 		originMiles: number,
@@ -98,7 +124,7 @@
 <div class="m-5 w-full rounded bg-slate-200 p-5 dark:bg-gray-900">
 	<h2 class="text-2xl font-extrabold">Saved Searches</h2>
 	{#if savedSearches.length}
-		<div class="flex flex-col justify-between h-full pb-4">
+		<div class="flex h-full flex-col justify-between pb-4">
 			<div>
 				{#each savedSearches as savedSearch, index}
 					{#if index <= 4}
@@ -107,11 +133,13 @@
 							<p></p>
 							<div class="flex flex-row justify-around">
 								<button
+									onclick={() => toggleText(savedSearch.textNotification, savedSearch.id)}
 									class="{savedSearch.textNotification
 										? active
 										: inactive} mx-1 my-3 rounded px-4 py-2 text-white">Text</button
 								>
 								<button
+									onclick={() => toggleEmail(savedSearch.emailNotification, savedSearch.id)}
 									class="{savedSearch.emailNotification
 										? active
 										: inactive} mx-1 my-3 rounded px-4 py-2 text-white">Email</button
@@ -131,14 +159,18 @@
 										)}
 									class="mx-1 my-3 rounded bg-blue-600 px-4 py-2 text-white">View</button
 								>
-								<button class="mx-1 my-3 rounded bg-blue-600 px-4 py-2 text-white">Delete</button>
+								<button
+									onclick={() => deleteRow(savedSearch.id, savedSearch.name)}
+									class="mx-1 my-3 rounded border border-red-600 px-4 py-2 text-red-600 hover:bg-red-600 hover:text-white"
+									>Delete</button
+								>
 							</div>
 						</div>
 					{/if}
 				{/each}
 			</div>
 
-			<div class="flex gap-3 flex-col md:flex-row ">
+			<div class="flex flex-col gap-3 md:flex-row">
 				<button
 					class="mx-50 my-3 w-full rounded bg-slate-800 px-5 py-2 text-white"
 					onclick={() => {
@@ -153,9 +185,9 @@
 	{:else}
 		<div class="flex flex-col items-center justify-center">
 			<div class="m-auto text-center text-4xl">No saved searches available</div>
-			<Button outline color="blue" class="m-auto mt-10 w-40" on:click={() => getRecords()}
-				>Refresh</Button
-			>
+			<Button outline color="blue" class="m-auto mt-10 w-40" on:click={refresh}>Refresh</Button>
 		</div>
 	{/if}
 </div>
+
+<DeleteConfirmationModal id={deleteID} name={deleteName} bind:deleteConfirmationShowing bind:savedSearches/>
