053d8f366f0e16ff706663ee72d30d68a75fafce
Michael Neal <mike@miken.dev>
2025-02-16 22:49:15 -0500

    create a small skeleton for single line text

---
+++ src/lib/components/Skeleton.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+import { fade } from "svelte/transition";
+</script>
+
+<div class="flex flex-col justify-end">
+<div class="m-4 flex flex-col justify-end" transition:fade>
+	<!-- Skeleton loading background with animation -->
+</div>
+	</div>
