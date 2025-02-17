f558a67d3cc132e143b6e2497335fafa4a963eb2
Michael Neal <mike@miken.dev>
2025-02-17 03:50:44 -0500

    update table row with correct value

--- src/lib/components/LoadTablev2.svelte
+++ src/lib/components/LoadTablev2.svelte
@@ -70,9 +70,6 @@
 <div
 	class="overflow-x-auto rounded-lg border border-gray-200 shadow-sm dark:bg-gray-800 dark:text-gray-100"
 >
-	{#if loggedIn}
-<div></div>
-	{/if}
 	<table class="w-full divide-y divide-gray-200">
 		<tbody class="divide-y divide-gray-200 bg-white">
 			{#each tableData as row, index}
@@ -154,8 +151,8 @@
 									</div>
 								</div>
 								<div>
-									<div class="text-xs font-medium text-gray-500 dark:text-gray-300">Agent</div>
-									<div class="mt-1 text-sm">AGENT NAME</div>
+									<div class="text-xs font-medium text-gray-500 dark:text-gray-300">Terminal</div>
+									<div class="mt-1 text-sm">{row.terminalName}</div>
 								</div>
 								<div>
 									<div class="text-xs font-medium text-gray-500 dark:text-gray-300">Phone #</div>
