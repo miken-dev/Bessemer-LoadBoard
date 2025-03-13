6c21c68c08a0a27eeb9497b3e8e949d419c9afb4
Michael Neal <mike@miken.dev>
2025-03-13 15:48:16 -0400

    remove sensitive info mask on sentry

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -15,7 +15,10 @@ Sentry.init({
 	replaysOnErrorSampleRate: 1.0,
 
 	// If you don't want to use Session Replay, just remove the line below:
-	integrations: [replayIntegration()]
+	integrations: [replayIntegration({
+		maskAllText: false, 
+		blockAllMedia: false,
+	})]
 });
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
