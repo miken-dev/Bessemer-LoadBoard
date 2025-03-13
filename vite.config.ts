881b14f082a361f58de0bdcd735f5cb39b7736bd
Michael Neal <mike@miken.dev>
2025-03-13 15:38:02 -0400

    add sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sentrySvelteKit({
+			sourceMapsUploadOptions: {
+				org: 'dennison-creative',
+				project: 'javascript-sveltekit'
+			}
+		}),
+		sveltekit()
+	],
 	server: {
 		host: true
-}
+	}
 });
