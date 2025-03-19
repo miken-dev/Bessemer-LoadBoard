b932f5b6775389ac472ce7cc49790311fe36138b
Michael Neal <mike@miken.dev>
2025-03-19 16:51:42 -0400

    reenable dev login button

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -33,8 +33,9 @@
 	</div>
 	<div class="flex">
 		<DarkMode class="pt-2 dark:text-gray-100" />
+		{#if dev}
 			<Button on:click={devLogin} color="blue">Log in (DEV)</Button>
-		{/if} -->
+		{/if} 
 		{#if loggedIn}
 			<Button size="sm" on:click={logout} color="blue">Log Out</Button>
 		{:else}
