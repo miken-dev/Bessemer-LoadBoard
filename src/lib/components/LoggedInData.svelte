d561a75eb5361f3228c5a9b95329b3a929a7afe5
Michael Neal <mike@miken.dev>
2025-04-03 09:16:51 -0400

    remove unneeded console log

--- src/lib/components/LoggedInData.svelte
+++ src/lib/components/LoggedInData.svelte
@@ -192,7 +192,6 @@
 	}
 
 	let filteredData = $derived.by((): TableDataTypes[] => {
-		console.log(`filterValues: ${filterValues.trailerTypesFilter}`)
 		return filterAndSortTableData(tableData, filterValues, locations, currentSort);
 	});
 	function numberWithCommas(number: number) {
