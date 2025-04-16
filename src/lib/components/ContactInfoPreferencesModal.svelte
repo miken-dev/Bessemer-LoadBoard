57894169da7663e2fa936736b54f4e712a355015
Michael Neal <mike@miken.dev>
2025-04-16 16:31:59 -0400

    update document styling

--- src/lib/components/ContactInfoPreferencesModal.svelte
+++ src/lib/components/ContactInfoPreferencesModal.svelte
@@ -21,11 +21,11 @@
 
 	$effect(() => {
 		if (contactInfoPreferencesModal) {
-			dialogElement?.showModal()
+			dialogElement?.showModal();
 		} else {
-			dialogElement?.close()
+			dialogElement?.close();
 		}
-	})
+	});
 
 	const PB = new PocketBase('https://bessemer-loadboard.pockethost.io');
 	async function getUserInfo() {
@@ -46,7 +46,6 @@
 		emailAddress = userInfo.email;
 		phoneNumber = userInfo.phone;
 	});
-
 
 	async function save() {
 		const record = await PB.collection('driver').update(userId, {
@@ -88,18 +87,19 @@
 		// All checks passed
 		return true;
 	}
+
 </script>
 
-	<dialog
-		class="relative z-10"
-		bind:this={dialogElement}
-		onkeydown={(e) => {
-			if (e.key === 'Escape') {
-				contactInfoPreferencesModal = false
-			}
-		}}
-	>
-		<!--
+<dialog
+	class="relative z-10"
+	bind:this={dialogElement}
+	onkeydown={(e) => {
+		if (e.key === 'Escape') {
+			contactInfoPreferencesModal = false;
+		}
+	}}
+>
+	<!--
     Background backdrop, show/hide based on modal state.
 
     Entering: "ease-out duration-300"
@@ -109,71 +109,71 @@
       From: "opacity-100"
       To: "opacity-0"
   -->
-		<div class="fixed inset-0 bg-gray-500/75 transition-opacity" aria-hidden="true"></div>
+	<div class="fixed inset-0 bg-gray-500/75 transition-opacity" aria-hidden="true"></div>
 
-		<div class="fixed inset-0 z-10 w-screen overflow-y-auto">
-			<div class="flex min-h-full items-center justify-center p-4 text-center sm:p-0">
-				<div
-					class="relative w-full transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:max-w-lg"
-				>
-					<div class="align-stretch bg-white px-4 pb-4 pt-5 dark:bg-gray-900 sm:p-6 sm:pb-4">
-						<div class="">
-							<div class="mt-3 text-left sm:mt-0">
-								<h3 class="text-base font-semibold text-gray-900 dark:text-white" id="modal-title">
-									Notification Preferences
-								</h3>
-								<div class="mt-2 flex flex-col gap-5">
-									<div class="flex flex-col">
-										<Label for="email" class="mb-2 block">Email Address</Label>
-										<Input
-											autofocus
-											id="email"
-											class="email grow"
-											placeholder=""
-											bind:value={emailAddress}
-										/>
-									</div>
-									<div class="flex flex-col">
-										<Label for="phone" class="mb-2 block">Phone Number</Label>
-										<Input
-											class="h-10 w-full rounded-md p-5"
-											id="phone"
-											placeholder=""
-											bind:value={phoneNumber}
-										/>
-									</div>
+	<div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+		<div class="flex min-h-full items-center justify-center p-4 text-center sm:p-0">
+			<div
+				class="relative w-full transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:max-w-lg"
+			>
+				<div class="align-stretch bg-white px-4 pb-4 pt-5 dark:bg-gray-900 sm:p-6 sm:pb-4">
+					<div class="">
+						<div class="mt-3 text-left sm:mt-0">
+							<h3 class="text-base font-semibold text-gray-900 dark:text-white" id="modal-title">
+								Notification Preferences
+							</h3>
+							<div class="mt-2 flex flex-col gap-5">
+								<div class="flex flex-col">
+									<Label for="email" class="mb-2 block">Email Address</Label>
+									<Input
+										autofocus
+										id="email"
+										class="email grow"
+										placeholder=""
+										bind:value={emailAddress}
+									/>
+								</div>
+								<div class="flex flex-col">
+									<Label for="phone" class="mb-2 block">Phone Number</Label>
+									<Input
+										class="h-10 w-full rounded-md p-5"
+										id="phone"
+										placeholder=""
+										bind:value={phoneNumber}
+									/>
 								</div>
 							</div>
 						</div>
 					</div>
-					<div class="gap-3 bg-gray-50 px-4 py-3 pb-7 dark:bg-gray-900 sm:flex sm:flex-row sm:px-6">
-						{#if !saveButtonEnable()}
-							<Button size="md" color="alternative" disabled>Save</Button>
-						{:else}
-							<Button
-								size="md"
-								class="saveButton"
-								color="blue"
-								onclick={async () => {
-									await save();
-									contactInfoPreferencesModal = false;
-								}}>Save</Button
-							>
-						{/if}
+				</div>
+				<div class="gap-3 bg-gray-50 px-4 py-3 pb-7 dark:bg-gray-900 sm:flex sm:flex-row sm:px-6">
+					{#if !saveButtonEnable()}
+						<Button size="md" color="alternative" disabled>Save</Button>
+					{:else}
 						<Button
 							size="md"
-							color="alternative"
-							on:click={async () => {
-								emailAddress = userInfo.email;
-								phoneNumber = userInfo.phone;
+							class="saveButton"
+							color="blue"
+							onclick={async () => {
+								await save();
 								contactInfoPreferencesModal = false;
-							}}>Cancel</Button
+							}}>Save</Button
 						>
-					</div>
+					{/if}
+					<Button
+						size="md"
+						color="alternative"
+						on:click={async () => {
+							emailAddress = userInfo.email;
+							phoneNumber = userInfo.phone;
+							contactInfoPreferencesModal = false;
+						}}>Cancel</Button
+					>
 				</div>
 			</div>
 		</div>
-	</dialog>
+	</div>
+</dialog>
 <!--
 <Modal
 	title="Notification Preferences"
